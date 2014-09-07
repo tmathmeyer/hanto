2fad26de545ab32bb4e383cfa1d0fca86e6ce282
Nilesh Patel <nileshp88@gmail.com>
2014-09-06 21:28:11 -0400

    added javadoc to stuff

--- src/hanto/otnah/common/HantoTile.java
+++ src/hanto/otnah/common/HantoTile.java
@@ -3,25 +3,50 @@ package hanto.otnah.common;
 import java.util.Collection;
 
 import hanto.common.HantoPiece;
-import hanto.otnah.common.Position;
 import hanto.common.HantoCoordinate;
 
+/**
+ * Class for handling the things pieces can do and things
+ * around them.
+ */
 public abstract class HantoTile implements HantoPiece{
 	Position position;
 	boolean played;
 	
+	/**
+	 * Makes the tile move to the given coordinates, regardless
+	 * of whether or not it is valid, make sure the position is
+	 * valid before calling this.
+	 * @param to The position to move to.
+	 */
 	public abstract void move(HantoCoordinate to);
 	
-	public abstract boolean isValidMove(Position to);
+	/**
+	 * Checks whether a given position is a valid position for the tile to move to.
+	 * @param to The position to check against.
+	 * @param game The current GameState object.
+	 * @return
+	 */
+	public abstract boolean isValidMove(Position to, GameState game);
 	
-	public abstract Collection<HantoCoordinate> getValidMoves();
-	
+	/**
+	 * Returns a collection of the adjacent non-empty tiles.
+	 * @return the collection of non-empty tiles
+	 */
 	public abstract Collection<HantoTile> getAdjacentPieces();
 	
+	/**
+	 * Gets Current Position
+	 * @return the Position object
+	 */
 	public Position getPosition(){
 		return position;
 	}
 	
+	/**
+	 * Tells whether the tile is in play or not.
+	 * @return true if on the board;
+	 */
 	public boolean inPlay(){
 		return played;
 	}
