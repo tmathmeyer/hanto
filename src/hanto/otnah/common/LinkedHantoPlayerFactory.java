d2b963d7960cf680ae4a8c95469f1305a3060a04
Ted Meyer <tmathmeyer@gmail.com>
2014-10-02 20:47:32 -0400

    refactor

--- src/hanto/otnah/common/LinkedHantoPlayerFactory.java
+++ src/hanto/otnah/common/LinkedHantoPlayerFactory.java
@@ -2,6 +2,7 @@ package hanto.otnah.common;
 
 import hanto.common.HantoPlayerColor;
 import hanto.otnah.common.pieces.Butterfly;
+import hanto.otnah.common.pieces.Crab;
 import hanto.otnah.common.pieces.Sparrow;
 import static hanto.otnah.common.util.CollectionUtils.makeInventory;
 import static hanto.otnah.common.util.CollectionUtils.with;
@@ -11,13 +12,41 @@ public class LinkedHantoPlayerFactory
 {
 	public static LinkedHantoPlayer makeGammaPlayers(HantoPlayerColor ... colors)
 	{
+		return makeLinkedPlayers(new PlayerGen(){
+			@Override
+			public LinkedHantoPlayer gen(HantoPlayerColor each) {
+				return makePlayer(each, makeInventory(HantoTile.class,
+						 				with(Butterfly.class, 1, each),
+						 				with(Sparrow.class, 5, each)));
+			}
+		});
+		
+		
+		
+		
+	}
+	
+	public static LinkedHantoPlayer makeDeltaPlayers(HantoPlayerColor ... colors)
+	{
+		return makeLinkedPlayers(new PlayerGen(){
+			@Override
+			public LinkedHantoPlayer gen(HantoPlayerColor each) {
+				return makePlayer(each, makeInventory(HantoTile.class,
+						 		 		with(Crab.class, 1, each),
+						 		 		with(Butterfly.class, 1, each),
+						 		 		with(Sparrow.class, 4, each)));
+			}
+		});
+		
+	}
+	
+	private static LinkedHantoPlayer makeLinkedPlayers(PlayerGen gen, HantoPlayerColor ... colors)
+	{
 		LinkedHantoPlayer first=null, last=null;
 		
 		for(HantoPlayerColor each : colors)
 		{
-			LinkedHantoPlayer current = makePlayer(each, makeInventory(HantoTile.class,
-														 with(Butterfly.class, 1, each),
-														 with(Sparrow.class, 5, each)));
+			LinkedHantoPlayer current = gen.gen(each);
 			current.setNextPlayer(last);
 			last = current;
 			if (first == null)
@@ -29,5 +58,10 @@ public class LinkedHantoPlayerFactory
 		first.setNextPlayer(last);
 		
 		return first;
+	}
+	
+	private static interface PlayerGen
+	{
+		public LinkedHantoPlayer gen(HantoPlayerColor hpc);
 	}
 }
